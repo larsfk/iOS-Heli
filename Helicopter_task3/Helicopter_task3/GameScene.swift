97b8c0677521fd26f5235717fb86d92239b6e5ae
larsfk <larsklingen@gmail.com>
2015-01-27 16:48:21 +0100

    Task 3 done. Multiple helicopters fly and collide. Good times.

--- Helicopter_task3/Helicopter_task3/GameScene.swift
+++ Helicopter_task3/Helicopter_task3/GameScene.swift
@@ -7,28 +7,44 @@
 //
 
 import SpriteKit
-import Darwin
 
 struct PhysicsCategory {
     static let None         : UInt32 = 0
     static let All          : UInt32 = UInt32.max
-    static let Player       : UInt32 = 0b1      // 1
+    static let Player       : UInt32 = 0x1 << 1
+    static let LeftEdge     : UInt32 = 0x1 << 2
+    static let RightEdge    : UInt32 = 0x1 << 3
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    let sheet = Heli()
-    let pi = CGFloat(M_PI)
     
+    // Initialize players as helicopters
+    let player1 = SKSpriteNode(texture: Heli().heli_1())
+    let player2 = SKSpriteNode(texture: Heli().heli_1())
+    let player3 = SKSpriteNode(texture: Heli().heli_1())
+    let player4 = SKSpriteNode(texture: Heli().heli_1())
+
     
     override func didMoveToView(view: SKView) {
+        // Sets background color
         backgroundColor = SKColor.whiteColor()
         
-        // Sets the world to be a physical world with gravity 0 and can have collitions (two physical bodies)
-        physicsWorld.gravity = CGVectorMake(0, 0)
-        physicsWorld.contactDelegate = self
+        /* World */
+        // Physics
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: view.frame)
+        self.physicsBody?.friction = 0.0
+        self.physicsWorld.gravity = CGVectorMake(0, 0)
+        // Contact
+        self.physicsWorld.contactDelegate = self
         
-        runAction(SKAction.repeatAction(SKAction.runBlock(addHeli), count: 2))
+        // Add walls
+        addWalls()
         
+        // Add helicopters
+        addHeli(player1)
+        addHeli(player2)
+        addHeli(player3)
+        addHeli(player4)
     }
     
     func random() -> CGFloat {
@@ -48,43 +64,74 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return num
     }
     
-    func addHeli(){
+    // Adds helicopter
+    func addHeli(player: SKSpriteNode){
         
-        let player = SKSpriteNode(texture: sheet.heli_1())
+        // Random variables to make the player spawn on a random spot
+        let x = self.random(min: CGFloat(10), max: CGFloat(500))
+        let y = self.random(min: CGFloat(10), max: CGFloat(500))
         
-        player.position = CGPoint(x: player.size.width / 2, y: size.height / 2)
         
+        /* Player */
+        player.position = CGPoint(x: x, y: y)
         addChild(player)
+        player.setScale(1.5)
         
-        var fly = SKAction.animateWithTextures(sheet.heli_fly(), timePerFrame: 0.1)
-        var flyAnim = SKAction.repeatAction(fly, count: Int(randomInt()))
-        
-        var flyRight = SKAction.moveToX(size.width, duration: flyAnim.duration)
-        var flyLeft = SKAction.moveToX(0, duration: flyAnim.duration)
-        
-        var mirrorDirection = SKAction.scaleXTo(1, y:1, duration: 0.0)
-        var resetDirection  = SKAction.scaleXTo(-1,  y:1, duration:0.0)
-        
-        var flyAndMoveRight = SKAction.group([resetDirection,flyAnim,flyRight])
-        var flyAndMoveLeft = SKAction.group([mirrorDirection, flyAnim,flyLeft])
-        
+        // Physics
         player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)  // Create a physical body 4 the sprite (rectangle)
-        player.physicsBody?.dynamic = true                                 // Dynamic -> you code motion, not the engine
-        player.physicsBody?.categoryBitMask = PhysicsCategory.Player      // Sets category bit mask to be monsterCategory
-        player.physicsBody?.contactTestBitMask = PhysicsCategory.Player // Which object to listen to (collition)
-        player.physicsBody?.collisionBitMask = PhysicsCategory.None        // Which objects should bounce of player
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.frame.size.height/2)
+        player.physicsBody?.friction = 0.0
+        player.physicsBody?.restitution = 1.0
+        player.physicsBody?.linearDamping = 0.0
+        player.physicsBody?.angularDamping = 0.0
+        player.physicsBody?.allowsRotation = true
         
-        player.runAction(SKAction.repeatActionForever(SKAction.sequence([flyAndMoveRight,flyAndMoveLeft])))
+        // Contact
+        player.physicsBody!.categoryBitMask = PhysicsCategory.Player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.Player
+        
+        // Freeze the helicopter, then shoot it in a direciton
+        player.physicsBody?.velocity = CGVectorMake(0,0) // Freeze the helicopter
+        player.physicsBody?.applyImpulse(CGVectorMake(-5,-5)) // Shoots the helicopter in a direction
+
+        
+        // Adds animation to the helicopter
+        player.runAction(SKAction.repeatActionForever(SKAction.animateWithTextures(Heli().heli_fly(), timePerFrame: 0.1)))
+
     }
     
-    // Removes the monster and the projectile when hit
-    func playerDidCollideWithPlayer(player1: SKSpriteNode, player2: SKSpriteNode){
-        println("Hit")
+    // Adds walls
+    func addWalls(){
+        /* Left */
+        let leftRect = CGRectMake(frame.origin.x, frame.origin.y, 1, frame.size.height)
+        let leftEdge = SKNode()
+        addChild(leftEdge)
+        
+        // Physics
+        leftEdge.physicsBody = SKPhysicsBody(edgeLoopFromRect: leftRect)
+        
+        // Contact
+        leftEdge.physicsBody!.categoryBitMask = PhysicsCategory.LeftEdge
+        leftEdge.physicsBody?.contactTestBitMask = PhysicsCategory.Player
+        
+        
+        /* Right */
+        let rightRect = CGRectMake(frame.size.width,frame.origin.y, 1,frame.size.height )
+        let rightEdge = SKNode()
+        addChild(rightEdge)
+        
+        // Physics
+        rightEdge.physicsBody = SKPhysicsBody(edgeLoopFromRect: rightRect)
+        
+        // Contact
+        rightEdge.physicsBody!.categoryBitMask = PhysicsCategory.RightEdge
+        rightEdge.physicsBody?.contactTestBitMask = PhysicsCategory.Player
+
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
+
         // Sets the two objects that will collide and sort them out
-        
         var firstBody: SKPhysicsBody
         var secondBody: SKPhysicsBody
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
@@ -95,43 +142,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             secondBody = contact.bodyA
         }
         
-        // Checks if its the monster and the projectile thats colliding, and then make the call!
-        
-        if ((firstBody.categoryBitMask & PhysicsCategory.Player != 0) &&
-            (secondBody.categoryBitMask & PhysicsCategory.Player != 0)) {
-                playerDidCollideWithPlayer(firstBody.node as SKSpriteNode, player2: secondBody.node as SKSpriteNode)
+        // Helicopter hits left egde -> flips the sprite
+        if firstBody.categoryBitMask == PhysicsCategory.Player && secondBody.categoryBitMask == PhysicsCategory.LeftEdge {
+            println("Hit LeftEdge")
+            var player = firstBody.node as SKSpriteNode
+            player.runAction(SKAction.scaleXTo(-1.5, y: 1.5, duration: 0.0))
+
+        }
+        // Helicopter hits right egde -> flips the sprite
+        if firstBody.categoryBitMask == PhysicsCategory.Player && secondBody.categoryBitMask == PhysicsCategory.RightEdge {
+            println("Hit RightEdge")
+            var player = firstBody.node as SKSpriteNode
+            player.runAction(SKAction.scaleXTo(1.5, y: 1.5, duration: 0.0))
         }
         
+        // Checks if two helicopters collide and then calls a function
+        if ((firstBody.categoryBitMask == PhysicsCategory.Player) &&
+            (secondBody.categoryBitMask == PhysicsCategory.Player)) {
+                playerDidCollideWithPlayer(firstBody.node as SKSpriteNode, player2: secondBody.node as SKSpriteNode)
+        }
     }
     
-    
-//        func moveHeli() {
-//    
-//            // Determine speed
-//            let actualSpeed = CGFloat(2.0)
-//    
-//            // Random Y axis
-//            let acutalY_1 = random(min: player.size.height/2, max: size.height - player.size.height/2)
-//            let acutalY_2 = random(min: player.size.height/2, max: size.height - player.size.height/2)
-//    
-//    
-//            // Create action
-//            let rightPoint = CGPoint(x: size.width - player.size.width / 2, y: acutalY_1)
-//            let actionMoveRight = SKAction.moveTo(rightPoint, duration: NSTimeInterval(actualSpeed))
-//    
-//            let leftPoint = CGPoint(x: 0, y: acutalY_2)
-//            let actionMoveLeft = SKAction.moveTo(leftPoint, duration: NSTimeInterval(actualSpeed))
-//    
-//            let moveToPoint = CGPoint(x: point.x, y: point.y)
-//            let actionMoveToPoint = SKAction.moveTo(point, duration: NSTimeInterval(actualSpeed))
-//    
-//            player.runAction(SKAction.sequence([rotation(from: player.position, to: moveToPoint), actionMoveToPoint]))
-//            
-//        }
-    
-    
-    
-    
-    
-    
+    // Flips the sprite after they collide
+    func playerDidCollideWithPlayer(player1: SKSpriteNode, player2: SKSpriteNode){
+        player1.runAction(SKAction.scaleXTo(player1.xScale * -1, y: 1.5, duration: 0.0))
+        player2.runAction(SKAction.scaleXTo(player2.xScale * -1, y: 1.5, duration: 0.0))
+    }
 }
